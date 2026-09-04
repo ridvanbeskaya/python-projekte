8a70864f180f84e9f2861f616c62aaeb12b56d24
ridvanbeskaya <ridvanbeskaya@gmx.de>
2026-09-04 18:52:24 +0200

    handwerkbetrieb_lager_v2

--- handwerkbetrieb_lager.py
+++ handwerkbetrieb_lager.py
@@ -1,8 +1,33 @@
 import csv
 
 with open("lagerbestand.csv","r") as datei:
-    reader = csv.DictReader(datei, delimiter=",")
+    reader = list(csv.DictReader(datei, delimiter=","))
+
+    def gesamtwert(lager_liste):
+        gesamt = 0
+        for produkt in lager_liste:
+         if float(produkt.get("menge")) > 0:
+            gesamt += float(produkt.get("menge"))*float(produkt.get("einzelpreis"))
+        return gesamt
+
+    print(gesamtwert(reader))
+
+    def mindestbestand(lager_liste):
+        mindestbestand_liste =[a["produkt"] for a in lager_liste if float(a["menge"])<float(a["mindestbestand"])]
+        return mindestbestand_liste
+    print(mindestbestand(reader))
+
+    def negative_werte(lager_liste):
+        negative_wert_liste = []
+        for a in lager_liste:
+            if float(a.get("menge")) < 0 or float(a.get("menge")) is None:
+                negative_wert_liste.append(a["produkt"])
+            elif float(a.get("einzelpreis")) < 0 or float(a.get("einzelpreis")) is None:
+                negative_wert_liste.append(a["produkt"])
+            elif float(a.get("menge")) < 0 or float(a.get("menge")) is None:
+                negative_wert_liste.append(a["produkt"])
+        return negative_wert_liste
+    print(negative_werte(reader))
 
 
-    for zeile in reader:
-        print(zeile)
+
